2c3ecdfea6bff64ff31cb36033420df59e0ae986
Reylyer <reylyercross@gmail.com>
2022-05-24 19:10:08 +0700

    cleaning

--- src/main.cpp
+++ src/main.cpp
@@ -27,51 +27,23 @@ Game *main_game;
 
 int WIN_SIZE[] = {1280, 720};
 
-const GLfloat light_ambient[] = { 0.5f, 0.5f, 0.5f, 0.0f }; 
-const GLfloat light_diffuse[] = { 1.0f, 1.0f, 1.0f, 1.0f }; 
-const GLfloat light_specular[] = { 1.0f, 1.0f, 1.0f, 1.0f }; 
-const GLfloat light_position[] = { 0.0f, 20.0f, 10.0f, 1.0f }; 
-const GLfloat mat_ambient[] = { 0.7f, 0.7f, 0.7f, 1.0f }; 
-const GLfloat mat_diffuse[] = { 0.8f, 0.8f, 0.8f, 1.0f }; 
-const GLfloat mat_specular[] = { 1.0f, 1.0f, 1.0f, 1.0f }; 
-const GLfloat high_shininess[] = { 100.0f };
-
 void initGL() {
-    glClearColor(0., .5, 0.5, 1.0);
-
-    // glEnable(GL_LIGHTING);
-    // glEnable(GL_LIGHT0);
-    // glEnable(GL_LIGHT1);
 
     glColorMaterial(GL_FRONT, GL_DIFFUSE);
     glEnable(GL_COLOR_MATERIAL);
     glEnable(GL_NORMALIZE);
-    // glEnable(GL_TEXTURE_2D);
 
     glDepthFunc(GL_LEQUAL);
     glEnable(GL_DEPTH_TEST);
 
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-    glEnable(GL_BLEND);
+    glEnable(GL_BLEND); 
 
-    // glLightfv(GL_LIGHT0, GL_AMBIENT, light_ambient); 
- 	// glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse); 
- 	// glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular); 
- 	// glLightfv(GL_LIGHT0, GL_POSITION, light_position); 
- 	// glMaterialfv(GL_FRONT, GL_AMBIENT, mat_ambient); 
- 	// glMaterialfv(GL_FRONT, GL_DIFFUSE, mat_diffuse); 
- 	// glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular); 
- 	// glMaterialfv(GL_FRONT, GL_SHININESS, high_shininess); 
-
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    
     main_game = new Game(WIN_SIZE[0], WIN_SIZE[1]);
 }
 
 void handleKeyPress(unsigned char key, int x, int y){
     main_game->keyboardPressCallback(key, x, y);
-    GLUT_KEY_UP
 }
 void handleKeyRelease(unsigned char key, int x, int y){
     main_game->keyboardReleaseCallback(key, x, y);
